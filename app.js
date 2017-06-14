9f0811f7e6431aa1a5af9ec5c71ff6bfa6f5e97c
Ai Shi <shiai8901@gmail.com>
2017-06-14 13:08:37 -0700

    added functions

--- app.js
+++ app.js
@@ -24,14 +24,67 @@ var headers = {
   'Content-Type': 'application/json'
 };
 
-// format is {testHandleName: {result: [], status: ""}}
-var testHandles = {};
+// format is {testHandleName: {sites: [], iterations: Number, result: [], status: ""}}
+var testInfo = {};
 
+// a list of testHandleNames
+var testHandles = [];
+
+/**
+ * generate unique handle
+ * @return {String} handle
+ */
+function generateTestHandle() {
+
+}
+
+/**
+ * validate input string
+ * @param {String} input
+ * @return {Boolean} is validate input
+ */
 function validateInput(input) {
 
 }
 
+/**
+ * do the test on sites
+ * @param {Array} sitesToTest
+ * @param {Number} iterations
+ */
 function test(sitesToTest, iterations) {
+
+}
+
+/**
+ * update test status when test is completed
+ * @param {String} testHandle
+ * @param {String} status
+ */
+function updateTestStatus(testHandle, status) {
+	testInfo[testHandle].status = status;
+}
+
+/**
+ * update test result when test is completed
+ * @param {String} testHandle
+ * @param {String} result
+ */
+function updateTestResult(testHandle, result) {
+	testInfo[testHandle].result = result;
+}
+
+/**
+ * save test result to disk
+ */
+function saveTestResult(testHandle, result, callback) {
+
+}
+
+/**
+ * remove test result every 24 hours
+ */
+function removeTestResultWorker() {
 
 }
 
@@ -45,7 +98,7 @@ const requestListener = function(req, res) {
 		// input format {sitesToTest: [http://www.google.com, http://cnn.com, http://espn.com], iterations: 10}
 		var input = '';
 		// need to generate unique testHandle
-		var testHandle = ;
+		var testHandle = generateTestHandle();
 		req.on('error', function(err) {
 			console.log('Error: ', err.message);
 		});
@@ -55,7 +108,8 @@ const requestListener = function(req, res) {
 		req.on('end', function() {
 			console.log(input);
 			if (validateInput(input)) {
-				testHandles[testHandle] = {result: [], status: "started"};
+				testInfo[testHandle] = {sites: input.sitesToTest, iterations: input.iterations, result: [], status: "started"};
+				testHandles.push(testHandle);
 				// run the function on the input;
 				// test(input.sitesToTest, input.iterations) {
 				// }				
@@ -65,9 +119,23 @@ const requestListener = function(req, res) {
 			}
 		});
 		res.writeHead(201, headers);
-		res.end({testHandle: testHandle, status: testHandles[testHandle].status});
+		res.end({testInfo: testHandle, status: testInfo[testHandle].status});
 	} else if (method = 'GET') {
-
+		if (url === 'allTests') {
+			res.writeHead(200, headers);
+			res.end(JSON.stringify({handles: testHandles}));
+		} else if (testInfo[url] === undefined) {
+			response.writeHead(404, headers);
+			response.end('Cannot find the test with the testHandle = ' + url);
+		} else {
+			if (testHandles[url].status === "finished") {
+				res.writeHead(400, headers);
+				res.end(JSON.stringify(testInfo[url].result));
+			} else {
+				res.writeHead(200, headers);
+				res.end({testHandle: url, status: testInfo[url].status});
+			}
+		}
 	}
 }
 
